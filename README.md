ddf3e7cb9d7feca1ed31674d50c7318a627e6362
TimL <tim93larsson@gmail.com>
2019-10-28 11:53:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VueWebShop
+ BackendVueApp
---
+++ src/main/java/com/vueApplication/VueWebShop/VueWebShopApplication.java
@@ -0,0 +1,13 @@
+package com.vueApplication.VueWebShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueWebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueWebShopApplication.class, args);
+	}
+
+}
